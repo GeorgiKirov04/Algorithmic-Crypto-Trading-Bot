a5a88f5c447d59d2395a32ef27942c3d40dbf9b5
ГеоргиКиров <georgikirov70@gmail.com>
2023-02-28 12:46:01 +0200

    Project

---
+++ main_trading_Bot.py
@@ -0,0 +1,16 @@
+# import ccxt
+# import time
+# import pandas as pd
+# # Initialize the exchange object
+# api_key = '63e202a27913d60001358478'
+# api_secret = '17272fac-ea80-4a67-ab6d-4cb2295da2be'
+
+
+
+# exchange = ccxt.kucoin({
+#     'enebleRateLimit': True,
+#     'apiKey': api_key,
+#     'secret': api_secret
+# })
+
+
